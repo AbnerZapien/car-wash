f2987b0cf743632795e881a479df003da404be1c
Abner Zapien <abnerzg@Abners-MacBook-Air.local>
2026-01-19 09:08:47 -0600

    core: add profile fields to User (email/name/avatar)

--- internal/users/core/users.go
+++ internal/users/core/users.go
@@ -9,4 +9,8 @@ type User struct {
 	Username  string    `json:"username" db:"username"`
 	Password  string    `json:"-" db:"password"`
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	Email     string    `db:"email" json:"email"`
+	FirstName string    `db:"first_name" json:"firstName"`
+	LastName  string    `db:"last_name" json:"lastName"`
+	AvatarURL string    `db:"avatar_url" json:"avatarUrl"`
 }
